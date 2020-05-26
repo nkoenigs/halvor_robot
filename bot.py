457d7b1c567a2d11f242fe9bbb7099a41841f621
Nathan Koenigsmark <ndkoenigsmark@gmail.com>
2020-05-26 04:36:14 -0400

    now welcomes newbies

--- bot.py
+++ bot.py
@@ -15,8 +15,17 @@ TOKEN = 'NzE0NzI3NjI3MTEzMzY1NTA0.' + 'XszTuQ.UbPBWYzelALUzm0LmfOK97op_wY'
 
 client = discord.Client()
 
+# print a console message to confirm connection
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+#Send a DM to welcome newbies to the sever
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f'Hallo {member.name}, welcome to the windowless room!')
+    await member.dm_channel.send(f'Be sure to check into the "roles" channel to give yourself a role.')
+
 
 client.run(TOKEN)
