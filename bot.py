e6ab90c4fbd13a0389a3fbce8e93e99b4dcf2cc1
Nathan Koenigsmark <ndkoenigsmark@gmail.com>
2020-05-26 04:58:48 -0400

    can maybe give roles(untested)

--- bot.py
+++ bot.py
@@ -9,23 +9,24 @@
 import os
 import discord
 
-# yes i know this is bad practice but my .env file wouldn't load an I'm no superman
-# just promise you won't read the next line okay?... okay.
-TOKEN = 'NzE0NzI3NjI3MTEzMzY1NTA0.' + 'XszTuQ.UbPBWYzelALUzm0LmfOK97op_wY'
 
 client = discord.Client()
+new_member_role = "Simple Ricks"
 
 # print a console message to confirm connection
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-#Send a DM to welcome newbies to the sever
+#Send a DM to welcome newbies to the sever while also giveing them a role
 @client.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(f'Hallo {member.name}, welcome to the windowless room!')
-    await member.dm_channel.send(f'Be sure to check into the "roles" channel to give yourself a role.')
+    role = member.guild.roles.get('386317626533609472')
+    await member.add_roles(role)
+    
 
-
-client.run(TOKEN)
+# yes i know this is bad practice but my .env file wouldn't load an I'm no superman
+# just promise you won't read the next line okay?... okay.
+client.run('NzE0NzI3NjI3MTEzMzY1NTA0.' + 'XszTuQ.UbPBWYzelALUzm0LmfOK97op_wY')
