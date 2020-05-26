bddcec870fd3c7a8951b5a3e554ea4a32ad54983
Nathan Koenigsmark <ndkoenigsmark@gmail.com>
2020-05-26 05:31:05 -0400

    now with a help command

--- bot.py
+++ bot.py
@@ -9,8 +9,13 @@
 import os
 import discord
 
+supported_commands = {
+    '>help',
+    '>hallo'
+}
 
 client = discord.Client()
+speaking = False
 new_member_role = "Simple Ricks"
 
 # print a console message to confirm connection
@@ -19,12 +24,28 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 #Send a DM to welcome newbies to the sever while also giveing them a role
+# TODO :test giveing role to a new account
 @client.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(f'Hallo {member.name}, welcome to the windowless room!')
     role = member.guild.roles.get('386317626533609472')
     await member.add_roles(role)
+
+@client.event
+async def on_message(message):
+
+    # ignore our own messages to prevent looping
+    if message.author == client.user:
+        return
+
+    # tell user all of our commands
+    if message.content.startswith('>help'):
+        await message.channel.send('Try typing one of the following bot commands:   ' + ',   '.join(supported_commands))
+
+    # the Halvor Persson Meme
+    if message.content.startswith('>hallo'):
+        await message.channel.send('Hallo I, Halvor Persson, (born 11 March 1966) am a Norwegian former ski jumper!')
     
 
 # yes i know this is bad practice but my .env file wouldn't load an I'm no superman
