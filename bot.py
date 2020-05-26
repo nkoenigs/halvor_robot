7278833e79450286fd72edfcf4876dd134a622ab
Nathan Koenigsmark <ndkoenigsmark@gmail.com>
2020-05-26 06:15:46 -0400

    now running on a command structure

--- bot.py
+++ bot.py
@@ -10,50 +10,29 @@ import os
 import discord
 from discord.ext import commands
 
-supported_commands = {
-    '>help',
-    '>hallo'
-}
-
-client = discord.Client()
-speaking = False
-new_member_role = "Simple Ricks"
-bot = commands.Bot(command_prefix='!')
+#client = discord.Client()
+bot = commands.Bot(command_prefix='>')
 
 # print a console message to confirm connection
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
 # Send a DM to welcome newbies to the sever while also giveing them a role
 # TODO :test giveing role to a new account
-@client.event
+@bot.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(f'Hallo {member.name}, welcome to the windowless room!')
     role = member.guild.roles.get('386317626533609472')
     await member.add_roles(role)
 
-# Handle all bot commands
-@client.event
-async def on_message(message):
+# the Halvor Persson Meme
+@bot.command(name = 'hallo', help = 'gives life changing information about our lord and savior')
+async def hallo_wurold(ctx):
+    await ctx.send('Hallo I, Halvor Persson, (born 11 March 1966) am a Norwegian former ski jumper!')
 
-    # ignore our own messages to prevent looping
-    if message.author == client.user:
-        return
-
-    # tell user all of our commands
-    if message.content.startswith('>help'):
-        await message.channel.send('Try typing one of the following bot commands:   ' + ',   '.join(supported_commands))
-
-    # the Halvor Persson Meme
-    if message.content.startswith('>hallo'):
-        await message.channel.send('Hallo I, Halvor Persson, (born 11 March 1966) am a Norwegian former ski jumper!')
-
-    # play the wet wet mud insta post
-    if message.content.startswith('>bae'):
-        
 
 # yes i know this is bad practice but my .env file wouldn't load an I'm no superman
 # just promise you won't read the next line okay?... okay.
-client.run('NzE0NzI3NjI3MTEzMzY1NTA0.' + 'XszTuQ.UbPBWYzelALUzm0LmfOK97op_wY')
+bot.run('NzE0NzI3NjI3MTEzMzY1NTA0.' + 'XszTuQ.UbPBWYzelALUzm0LmfOK97op_wY')
