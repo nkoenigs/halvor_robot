6dd80b9f6c012d601c12452dac81fc4e4b02d330
Nathan Koenigsmark <ndkoenigsmark@gmail.com>
2020-05-26 04:29:11 -0400

    can now connect to discord and do nothing

---
+++ bot.py
@@ -0,0 +1,22 @@
+# Halvor Persson (born 11 March 1966) is a Norwegian former ski jumper.
+
+# Nathan D. Koenigsmark
+# discord bot for many things
+
+# dependencies:
+# $ pip install -U discord.py
+
+import os
+import discord
+
+# yes i know this is bad practice but my .env file wouldn't load an I'm no superman
+# just promise you won't read the next line okay?... okay.
+TOKEN = 'NzE0NzI3NjI3MTEzMzY1NTA0.Xsy4eA.TmgLM1scrNJXli9VFIu5i_aSIAU'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
