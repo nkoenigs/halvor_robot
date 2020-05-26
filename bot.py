eca772b1ff71db39b7e50a45ed2aefea3ffb6a0b
Nathan Koenigsmark <ndkoenigsmark@gmail.com>
2020-05-26 06:05:30 -0400

    right berfore switching to bot commands

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@
 
 import os
 import discord
+from discord.ext import commands
 
 supported_commands = {
     '>help',
@@ -17,13 +18,14 @@ supported_commands = {
 client = discord.Client()
 speaking = False
 new_member_role = "Simple Ricks"
+bot = commands.Bot(command_prefix='!')
 
 # print a console message to confirm connection
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-#Send a DM to welcome newbies to the sever while also giveing them a role
+# Send a DM to welcome newbies to the sever while also giveing them a role
 # TODO :test giveing role to a new account
 @client.event
 async def on_member_join(member):
@@ -32,6 +34,7 @@ async def on_member_join(member):
     role = member.guild.roles.get('386317626533609472')
     await member.add_roles(role)
 
+# Handle all bot commands
 @client.event
 async def on_message(message):
 
@@ -46,7 +49,10 @@ async def on_message(message):
     # the Halvor Persson Meme
     if message.content.startswith('>hallo'):
         await message.channel.send('Hallo I, Halvor Persson, (born 11 March 1966) am a Norwegian former ski jumper!')
-    
+
+    # play the wet wet mud insta post
+    if message.content.startswith('>bae'):
+        
 
 # yes i know this is bad practice but my .env file wouldn't load an I'm no superman
 # just promise you won't read the next line okay?... okay.
